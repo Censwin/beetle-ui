d042fc264361ca6e0c5dab2595c697427fe4caec
Censwin <704334497@qq.com>
2021-10-11 00:50:43 +0800

    button

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,39 @@
 /*
  * @Author: Censwin
  * @Date: 2021-10-08 23:19:50
- * @LastEditTime: 2021-10-09 18:05:13
+ * @LastEditTime: 2021-10-11 00:11:52
  * @Description:
  * @FilePath: /whale-design/src/App.tsx
  */
-import React from 'react'
-import Button, { ButtonType, ButtonSize } from './components/Button/button'
+import React from "react";
+import Button, { ButtonType, ButtonSize } from "./components/Button/button";
 function App() {
   return (
-    <div style={{ padding: '20px' }}>
-      <Button size={ButtonSize.Large} btnType={ButtonType.Primary}>
-        test
+    <div style={{ padding: "20px" }}>
+      <Button btnType={ButtonType.Primary}>Primary</Button>
+      <Button size={ButtonSize.Large} btnType={ButtonType.Default}>
+        Default
+      </Button>
+      <Button size={ButtonSize.Small} btnType={ButtonType.Warring}>
+        Warring
+      </Button>
+      <Button
+        size={ButtonSize.Large}
+        btnType={ButtonType.Danger}
+        disabled={true}
+      >
+        Danger
+      </Button>
+      <Button
+        size={ButtonSize.Large}
+        btnType={ButtonType.Link}
+        disabled={false}
+      >
+        Link
       </Button>
       {/* <Button btnType={ButtonType.Link}>Link</Button> */}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Button/button.tsx
+++ src/components/Button/button.tsx
@@ -1,43 +1,53 @@
-import classNames from 'classnames'
-import React from 'react'
+/*
+ * @Author: Censwin
+ * @Date: 2021-10-10 22:48:09
+ * @LastEditTime: 2021-10-11 00:06:24
+ * @Description:
+ * @FilePath: /whale-design/src/components/Button/button.tsx
+ */
+import classNames from "classnames";
+import React from "react";
 
 export enum ButtonSize {
-  Large = 'lg',
-  Small = 'sm',
+  Large = "lg",
+  Small = "sm",
 }
 
 export enum ButtonType {
-  Primary = 'primary',
-  Default = 'default',
-  Warring = 'warring',
-  Danger = 'danger',
-  Link = 'link',
+  Primary = "primary",
+  Default = "default",
+  Warring = "warring",
+  Danger = "danger",
+  Link = "link",
 }
 
 interface IButtonProps {
-  className?: string
-  disabled?: boolean
-  size?: ButtonSize
-  btnType?: ButtonType
-  children?: React.ReactNode
-  href?: string
+  className?: string;
+  disabled?: boolean;
+  size?: ButtonSize;
+  btnType?: ButtonType;
+  children?: React.ReactNode;
+  href?: string;
 }
 
 const Button: React.FC<IButtonProps> = (props) => {
-  const { btnType, disabled: _disabled, size, children, href: _href } = props
-  const classes = classNames('btn', {
-    [`btn-${btnType}`]: btnType,
-    [`btn-${size}`]: size,
-    disabled: _disabled,
-  })
+  const { btnType, disabled: _disabled, size, children, href: _href } = props;
+  const classes =
+    btnType === ButtonType.Link
+      ? "btn-link"
+      : classNames("btn", {
+          [`btn-${btnType}`]: btnType,
+          [`btn-${size}`]: size,
+          disabled: _disabled,
+        });
   return (
     <button className={classes} disabled={_disabled}>
       <span>{children}</span>
     </button>
-  )
-}
+  );
+};
 Button.defaultProps = {
   disabled: false,
   btnType: ButtonType.Default,
-}
-export default Button
+};
+export default Button;
