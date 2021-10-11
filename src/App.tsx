c4b48e73d0504ed154e8398cb50e02cdcaff3475
kyle <704334497@qq.com>
2021-10-11 14:46:13 +0800

    button

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,23 @@
 /*
  * @Author: Censwin
  * @Date: 2021-10-08 23:19:50
- * @LastEditTime: 2021-10-11 00:11:52
+ * @LastEditTime: 2021-10-11 14:34:57
  * @Description:
  * @FilePath: /whale-design/src/App.tsx
  */
-import React from "react";
-import Button, { ButtonType, ButtonSize } from "./components/Button/button";
+import React from 'react'
+import Button, { ButtonType, ButtonSize } from './components/Button/button'
 function App() {
   return (
-    <div style={{ padding: "20px" }}>
-      <Button btnType={ButtonType.Primary}>Primary</Button>
+    <div style={{ padding: '20px' }}>
+      <Button
+        btnType={ButtonType.Primary}
+        onClick={() => {
+          alert(1)
+        }}
+      >
+        Primary
+      </Button>
       <Button size={ButtonSize.Large} btnType={ButtonType.Default}>
         Default
       </Button>
@@ -25,15 +32,16 @@ function App() {
         Danger
       </Button>
       <Button
-        size={ButtonSize.Large}
         btnType={ButtonType.Link}
-        disabled={false}
+        disabled={true}
+        href="www.baidu.com"
+        target="_blank"
       >
         Link
       </Button>
       {/* <Button btnType={ButtonType.Link}>Link</Button> */}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/components/Button/button.tsx
+++ src/components/Button/button.tsx
@@ -1,53 +1,91 @@
 /*
  * @Author: Censwin
  * @Date: 2021-10-10 22:48:09
- * @LastEditTime: 2021-10-11 00:06:24
+ * @LastEditTime: 2021-10-11 14:44:24
  * @Description:
  * @FilePath: /whale-design/src/components/Button/button.tsx
  */
-import classNames from "classnames";
-import React from "react";
+import classNames from 'classnames'
+import * as React from 'react'
 
 export enum ButtonSize {
-  Large = "lg",
-  Small = "sm",
+  Large = 'lg',
+  Small = 'sm',
 }
 
 export enum ButtonType {
-  Primary = "primary",
-  Default = "default",
-  Warring = "warring",
-  Danger = "danger",
-  Link = "link",
+  Primary = 'primary',
+  Default = 'default',
+  Warring = 'warring',
+  Danger = 'danger',
+  Link = 'link',
 }
 
-interface IButtonProps {
-  className?: string;
-  disabled?: boolean;
-  size?: ButtonSize;
-  btnType?: ButtonType;
-  children?: React.ReactNode;
-  href?: string;
+interface IBaseButtonProps {
+  className?: string
+  disabled?: boolean
+  size?: ButtonSize
+  btnType?: ButtonType
+  children?: React.ReactNode
+  href?: string
 }
+type NativeButtonAttr = IBaseButtonProps &
+  React.ButtonHTMLAttributes<HTMLElement>
+type NativeAnchorAttr = IBaseButtonProps &
+  React.AnchorHTMLAttributes<HTMLElement>
+export type ButtonProps = Partial<NativeButtonAttr & NativeAnchorAttr>
 
-const Button: React.FC<IButtonProps> = (props) => {
-  const { btnType, disabled: _disabled, size, children, href: _href } = props;
-  const classes =
-    btnType === ButtonType.Link
-      ? "btn-link"
-      : classNames("btn", {
-          [`btn-${btnType}`]: btnType,
-          [`btn-${size}`]: size,
-          disabled: _disabled,
-        });
+const Button: React.FC<ButtonProps> = (props) => {
+  const {
+    btnType,
+    disabled: _disabled,
+    size,
+    children,
+    href: _href,
+    className: userDefinedClassName,
+    ...NativeAttr
+  } = props
+  const classes = classNames('btn', {
+    [`btn-${btnType}`]: btnType,
+    [`btn-${size}`]: size,
+    disabled: _disabled,
+    userDefinedClassName,
+  })
+
+  const handleClick = (
+    e: React.MouseEvent<HTMLButtonElement | HTMLAnchorElement, MouseEvent>
+  ) => {
+    const { onClick, disabled } = props
+    if (disabled) {
+      e.preventDefault()
+      return
+    }
+    ;(
+      onClick as React.MouseEventHandler<HTMLButtonElement | HTMLAnchorElement>
+    )?.(e)
+  }
+
+  if (_href) {
+    return (
+      <a href={_href} className={classes} {...NativeAttr} onClick={handleClick}>
+        {children}
+      </a>
+    )
+  }
   return (
-    <button className={classes} disabled={_disabled}>
+    <button
+      className={classes}
+      disabled={_disabled}
+      {...NativeAttr}
+      onClick={handleClick}
+    >
       <span>{children}</span>
     </button>
-  );
-};
+  )
+}
+
 Button.defaultProps = {
   disabled: false,
   btnType: ButtonType.Default,
-};
-export default Button;
+}
+export default Button
