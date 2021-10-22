2e4ebcaea7402f0a14486995872c6c8da25d9e93
kyle <704334497@qq.com>
2021-10-22 18:25:03 +0800

    pack

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,8 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './styles/index.scss'
-import App from './App'
-import reportWebVitals from './reportWebVitals'
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-)
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals()
+export { default as Button } from './components/Button'
+export { default as Menu } from './components/Menu'
+export { default as Alert } from './components/Alert'
+export { default as AutoComplete } from './components/AutoComplete'
+export { default as Icon } from './components/Icon'
+export { default as Input } from './components/Input'
+export { default as Progress } from './components/Progress'
+export { default as Upload } from './components/Upload'
